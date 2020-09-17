07ae0652605f52db56b4a12d39dc141c0a8236a7
juliocesar06 <julio06cesar11@gmail.com>
2020-09-16 23:05:02 -0300

    testando os arquivos co  pytest no travis

---
+++ libpythonpro/tests/numero.py
@@ -0,0 +1,4 @@
+def par_impar(n):
+    if n % 2 == 0:
+        return'par'
+    return 'impar'
---
+++ libpythonpro/tests/test_exemplo.py
@@ -0,0 +1,11 @@
+def test_int():
+    assert 1 == 1
+
+def test_flout():
+    assert 0.222 != 0
+
+def soma(a,b):
+    return a + b
+
+def test_soma():
+    assert soma(1,2) == 3
